bc1567845102905942b1b7a492585112369e74cb
Bobby Wertman <rwertman@polaris.cs.wcu.edu>
2011-11-02 21:59:01 -0400

    Moved messages into lib folder.

---
+++ messages/join.go
@@ -0,0 +1,24 @@
+package main
+
+import "strings"
+
+type m_join struct {
+	channel string
+}
+
+func NewJoinMessage(channel string) {
+	return m_join{channel}
+}
+
+func (m m_join) String() string {
+	if m.channel == "" {
+		return ":source JOIN 0"
+	}
+	s := ":source JOIN :"
+	if strings.TrimLeft(m.channel, "#") == m.channel {
+		s += "#" + m.channel
+	} else {
+		s += m.channel
+	}
+	return s
+}
